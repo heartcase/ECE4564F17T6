1451b438d5681c99d7b971f620e9ca2bdfed8c2d
Boyang Li <beyongl@vt.edu>
2017-12-01 22:55:22 -0500

    QR Scanner Implementation

--- Android/project/app/src/main/java/com/example/pc/parkingsystem/MainActivity.java
+++ Android/project/app/src/main/java/com/example/pc/parkingsystem/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.pc.parkingsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View.OnClickListener handle = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setBeepEnabled(false);
+                integrator.setCameraId(0);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        };
+        findViewById(R.id.button).setOnClickListener(handle);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            String scannedData = result.getContents();
+            if (scannedData != null) {
+                Toast.makeText(MainActivity.this,scannedData,Toast.LENGTH_LONG).show();
+            } else {
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
+
+
+
