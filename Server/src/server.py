e9f0fd33bc42723c026eeacb1c8cc70b8aff5bad
Teng Li <tengli@vt.edu>
2017-12-03 11:41:45 -0500

    outline of flask

--- Server/src/server.py
+++ Server/src/server.py
@@ -2,18 +2,36 @@ from flask import Flask
 from flask_restful import Resource, Api
 from flask import request
 
-class PARKING(Resource):
+class LOGIN(Resource):
     def get(self):
         #get functions here
         return {}
-    def post(self):
+
+class LIST(Resource):
+    def get(self):
+        #get functions here
+        return {}
+
+class PARKING(Resource):
+    def get(self,id):
+        #get functions here
+        return {}
+    def post(self,id):
         # post functions here
+        return {}
+
+class USER(Resource):
+    def get(self,uid):
+        #get functions here
         return {}
 
 def flaskService():
     app = Flask(__name__)
     api = Api(app)
-    api.add_resource(PARKING, '/parking')
+    api.add_resource(LOGIN, '/Login')
+    api.add_resource(LIST, '/ParkingSpots')
+    api.add_resource(PARKING, '/ParkingSpots/<id>')
+    api.add_resource(USER, '/User/<uid>')
     app.run(host='0.0.0.0')
 
 if __name__ == '__main__':
